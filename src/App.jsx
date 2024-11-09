1c90173703c650205ba2876b52535f5675d685fd
Stephmdavis1 <125842540+Stephmdavis1@users.noreply.github.com>
2024-11-09 12:47:53 -0500

    changed the route name for gh-pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
  
   return (
     <>
-      <BrowserRouter basename="/2024-Portfolio/">
+      <BrowserRouter basename="/portfolio/">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/work" element={<Work />} />
