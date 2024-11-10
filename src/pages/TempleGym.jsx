f82f477cbe39a4947cdbcd1a6138e973537b5f8a
Stephmdavis1 <125842540+Stephmdavis1@users.noreply.github.com>
2024-11-09 20:28:56 -0500

    temple mobile responsive

--- src/pages/TempleGym.jsx
+++ src/pages/TempleGym.jsx
@@ -1,6 +1,6 @@
-import mockup from "../assets/ProjectsDataImages/Sippers/sippers2.png";
+import temple_mockup from "../assets/ProjectsDataImages/Temple/Temple3.png";
 import templebackground from "../assets/ProjectsDataImages/Temple/IMG_4502.png";
-import templevid from "../assets/ProjectsDataImages/Temple/TempleVid.mp4";
+import temple_mockup2 from "../assets/ProjectsDataImages/Temple/Temple4.png";
 import TempleChart from "../assets/ProjectsDataImages/Temple/TempleChart.png";
 import "../components/Work/Temple/TempleGym.css";
 
@@ -20,36 +20,42 @@ const TempleGym = () => {
     <>
       <div className="port__container">
         <div className="temple__container1"></div>
+        <div className="temple__container3"></div>
         <div className="temple__container2">
-          
-            <h1 className="project__title1">Temple Gym & Fitness</h1>
-          
+          <h1 className="temple__title">Temple Gym & Fitness</h1>
+
           <a href="/work" className="back__button">
             back to all projects
           </a>
-          <div id="hero__details1">
+          <div id="hero__details1_temple">
             <h4 className="herotitle__name">Temple Gym & Fitness </h4>
             <p className="para__1">
               <i>Noun:</i> Temple Gym and Fitness is Jacksonville's #1 training
               center.
             </p>
-            <p className="para__2_temple">
+            <p className="para__2">
               <i>Role:</i> Researcher / Branding / Photographer / Interface
               Design
               <br />
-              <a href="https://www.templegymandfitness.com/" target="_blank" className="btn2 temple_btn">Visit Website</a>
+              <a
+                href="https://www.templegymandfitness.com/"
+                target="_blank"
+                className="btn2 temple_btn"
+              >
+                Visit Website
+              </a>
             </p>
           </div>
         </div>
       </div>
 
-      <div className="brand__box_temple2">
-        <div className="brand__left">
+      <div className="brand__box_temple">
+        <div className="brand__left_temple">
           <h3>Building the Brand Vision</h3>
         </div>
-        <div className="brand__right">
-          <h6 className="brand__title2">The Challenge</h6>
-          <div className="brand__description">
+        <div className="brand__right_temple">
+          <h6 className="brand__right2_temple">The Challenge</h6>
+          <div className="brand__description_temple">
             <p>
               Temple Gym and Fitness is a staple gym in the Jacksonville local
               community. With a specialized facility and science backed
@@ -57,12 +63,22 @@ const TempleGym = () => {
               to succeed.
             </p>
             <br />
-            <p>At it's core, Temple Gym and Fitness is dedicated to promote a healthy lifestyle for the community. Offering science based programs, group fitness classes, sport specific training and more, this gym is more than capable to handle all fitness ideas and needs. The company's indoor and outdoor training area provides the space needed for the development of our youth and local community.</p>
+            <p>
+              At it's core, Temple Gym and Fitness is dedicated to promote a
+              healthy lifestyle for the community. Offering science based
+              programs, group fitness classes, sport specific training and more,
+              this gym is more than capable to handle all fitness ideas and
+              needs. The company's indoor and outdoor training area provides the
+              space needed for the development of our youth and local community.
+            </p>
             <br />
             <p>
               I was approached by the owner and the board members to do a total
               rebrand to help them to attract new customers and to do a revamp
-              of their current website. With a focus on improving the user experience and streamlining the content structure, I was able to create a website that effectively communicated Temple's value proposition and showcased their expertise in fitness.
+              of their current website. With a focus on improving the user
+              experience and streamlining the content structure, I was able to
+              create a website that effectively communicated Temple's value
+              proposition and showcased their expertise in fitness.
             </p>
             <br />
             <p>
@@ -81,55 +97,90 @@ const TempleGym = () => {
           </div>
         </div>
       </div>
-      <div className="img__container">
-        <img className="brand__temple" alt="" />
+      <div className="img__container_temple">
+        <img src={temple_mockup2} alt="" />
       </div>
 
-      <div className="port__container">
-        <div className="brand__box_temple">
-          <div className="brand__right">
-          <h5>The Focus</h5>
-            <p>
-            With these challenges in mind, I narrowed in on two questions that would guide my approah to finding a solution:
-            </p>
-            <p>- Who are our interested new members?</p>
-            <p>- What information does a successful web app view contain?</p>
-    
-            <h5>The Design Process</h5>
-            <p>
-            My first step was to analyze Temple's existing website to understand how their content was organized. I found that the website had too little information and was not well-organized, making it difficult for users to find the information they were looking for. To address this issue, I started by grouping the information into categories and subcategories, making it easier for users to navigate the website.
-            </p>
-            <br />
-            <p>In addition to categorising the content, I also prioritized the information on the homepage. I reduced the amount of content focusing only on the most important information. I wanted to ensure that users would immediately find the most relevant and important information when they landed on the website. By doing this, we were able to create a more streamlined user experience that reduced cognitive overlaod and made it easier to find what they needed.</p>
-            <br />
-            <p>I kicked off the wireframing process by doing a complete rebranding of the website. Starting with the homepage and then working to the other pages for the website, I made sure to keep the main questions in mind.
-        </p>
+      <div className="port__container_temple">
+        <div className="brand__box_temple2">
+          <div className="brand__right_temple">
+            <h5 className="brand__right2_temple">The Focus</h5>
+            <div className="brand__description_temple">
+              <p>
+                With these challenges in mind, I narrowed in on two questions
+                that would guide my approah to finding a solution:
+              </p>
+              <br />
+              <p>- Who are our interested new members?</p>
+              <br />
+              <p>- What information does a successful web app view contain?</p>
+            </div>
+
+            <h5 className="brand__right2_temple">The Design Process</h5>
+            <div className="brand__description_temple">
+              <p>
+                My first step was to analyze Temple's existing website to
+                understand how their content was organized. I found that the
+                website had too little information and was not well-organized,
+                making it difficult for users to find the information they were
+                looking for. To address this issue, I started by grouping the
+                information into categories and subcategories, making it easier
+                for users to navigate the website.
+              </p>
+              <br />
+              <p>
+                In addition to categorising the content, I also prioritized the
+                information on the homepage. I reduced the amount of content
+                focusing only on the most important information. I wanted to
+                ensure that users would immediately find the most relevant and
+                important information when they landed on the website. By doing
+                this, we were able to create a more streamlined user experience
+                that reduced cognitive overload and made it easier to find what
+                they needed.
+              </p>
+              <br />
+              <p>
+                I kicked off the wireframing process by doing a complete
+                rebranding of the website. Starting with the homepage and then
+                working to the other pages for the website, I made sure to keep
+                the main questions in mind.
+              </p>
+            </div>
           </div>
           <div className="brand__left1">
-            <img src={TempleChart} alt="chart of users" className="chart" />
+            <img
+              src={TempleChart}
+              alt="chart of users"
+              className="chart_temple"
+            />
           </div>
         </div>
-        <div className="rebrand__container1">
-        
+
+        <div className="img__container_temple">
+          <img src={temple_mockup} alt="" />
         </div>
 
-        <div className="img__container">
-          <img className="brand__temple2" alt="" />
-        </div>
-
-
-        <div className="brand__box3">
-          <div className="brand__left">
+        <div className="brand__box3_temple">
+          <div className="brand__left_temple2">
             <h3>The Conclusion</h3>
           </div>
           <div className="brand__right">
-            <article className="temple-brand__title3">
+            <article className="brand__description_temple">
+              <br />
               <p>
-              Creating a user-centered design for Temple was both a challenging and a rewarding experience. By conducting UX research, analysing user data, and creating UX stategy, I was able to identify the key pain points and opportunities for the website. The wireframing and prototyping process allowed me to test and refine the design to ensure it was user-frieldnt and easy to use. The final UI design was modern, vibrant, and consistent. This provided users with a seamless and enjoyable experience when discoving new experiences. I love how the
-                website is taking a new client through the process of becoming a
-                member through the use of images and the programs the gym
-                offers. This responsive website evolved and expanded to include
-                and highlight all the features the gym has to offer.
+                Creating a user-centered design for Temple was both a
+                challenging and a rewarding experience. By conducting UX
+                research, analyzing user data, and creating UX strategy, I was
+                able to identify the key pain points and opportunities for the
+                website. The wireframing and prototyping process allowed me to
+                test and refine the design to ensure it was user-friendly and
+                easy to use. The final UI design was modern, vibrant, and
+                consistent. This provided users with a seamless and enjoyable
+                experience when discovering new experiences. I love how
+                thewebsite is taking a new client through the process of
+                becoming a member through the use of images and the programs the
+                gym offers. This responsive website evolved and expanded to
+                include and highlight all the features the gym has to offer.
               </p>
               <br />
               <p>
