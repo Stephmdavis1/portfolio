00f68cfa7d49290022ac6c6f638163882c8e669b
Stephmdavis1 <125842540+Stephmdavis1@users.noreply.github.com>
2025-01-07 18:35:54 -0500

    Temple mobile view updates

--- src/pages/TempleGym.jsx
+++ src/pages/TempleGym.jsx
@@ -196,7 +196,7 @@ const TempleGym = () => {
         </div>
 
         <div>
-          <img src={templebackground} alt="" className="sippers__hero" />
+          <img src={templebackground} alt="" className="temple__hero" />
         </div>
       </div>
     </>
